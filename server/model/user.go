488052174fae999cc85bc77626c2d54fc0951d60
ThiwankaS <thivankas@outlook.com>
2026-01-26 20:50:11 +0200

    connecting mongoDB atlas to the backend

---
+++ server/model/user.go
@@ -0,0 +1,14 @@
+package model
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Email     string             `bson:"email" json:"email"`
+	Name      string             `bson:"name" json:"name"`
+	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
+}
